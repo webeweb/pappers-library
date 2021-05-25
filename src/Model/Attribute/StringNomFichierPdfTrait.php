3eb592a76de8d722c588ff0110204d5228349cad
webeweb <support@webeweb.fr>
2021-05-25 19:09:15 +0200

    Add string nom fichier PDF trait

---
+++ src/Model/Attribute/StringNomFichierPdfTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model\Attribute;
+
+/**
+ * String Nom fichier PDF trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model\Attribute
+ */
+trait StringNomFichierPdfTrait {
+
+    /**
+     * Nom fichier PDF.
+     *
+     * @var string|null
+     */
+    private $nomFichierPdf;
+
+    /**
+     * Get the nom fichier PDF.
+     *
+     * @return string|null Returns the nom fichier PDF.
+     */
+    public function getNomFichierPdf(): ?string {
+        return $this->nomFichierPdf;
+    }
+
+    /**
+     * Set the nom fichier PDF.
+     *
+     * @param string|null $nomFichierPdf The nom fichier PDF.
+     * @return self Returns this instance.
+     */
+    public function setNomFichierPdf(?string $nomFichierPdf): self {
+        $this->nomFichierPdf = $nomFichierPdf;
+        return $this;
+    }
+}
