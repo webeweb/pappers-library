a06b3cea2296832b1caa9726d74bd97f0c839480
webeweb <support@webeweb.fr>
2021-05-25 19:10:29 +0200

    Add string prénom trait

---
+++ tests/Model/Attribute/StringPrenomTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model\Attribute;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Model\Attribute\TestStringPrenomTrait;
+
+/**
+ * String Prénom trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model\Attribute
+ */
+class StringPrenomTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setPrenom() method().
+     *
+     * @return void
+     */
+    public function testSetPrenom(): void {
+
+        $obj = new TestStringPrenomTrait();
+
+        $obj->setPrenom("prenom");
+        $this->assertEquals("prenom", $obj->getPrenom());
+    }
+}
