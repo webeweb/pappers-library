d967fa015e128d98c5cd0fb7c8ad22a8c604a64b
webeweb <support@webeweb.fr>
2021-05-25 19:08:23 +0200

    Add string date dépôt formaté trait

---
+++ tests/Model/Attribute/StringDateDepotFormateTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model\Attribute;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Model\Attribute\TestStringDateDepotFormateTrait;
+
+/**
+ * String Date dépôt formaté trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model\Attribute
+ */
+class StringDateDepotFormateTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setDateDepotFormate() method().
+     *
+     * @return void
+     */
+    public function testSetDateDepotFormate(): void {
+
+        $obj = new TestStringDateDepotFormateTrait();
+
+        $obj->setDateDepotFormate("dateDepotFormate");
+        $this->assertEquals("dateDepotFormate", $obj->getDateDepotFormate());
+    }
+}
