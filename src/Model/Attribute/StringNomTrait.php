5fe2a0dd3b42ff86c32df6093aa35d4559bf087c
webeweb <support@webeweb.fr>
2021-05-25 19:10:19 +0200

    Add string nom trait

---
+++ src/Model/Attribute/StringNomTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model\Attribute;
+
+/**
+ * String Nom trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model\Attribute
+ */
+trait StringNomTrait {
+
+    /**
+     * Nom.
+     *
+     * @var string|null
+     */
+    private $nom;
+
+    /**
+     * Get the nom.
+     *
+     * @return string|null Returns the nom.
+     */
+    public function getNom(): ?string {
+        return $this->nom;
+    }
+
+    /**
+     * Set the nom.
+     *
+     * @param string|null $nom The nom.
+     * @return self Returns this instance.
+     */
+    public function setNom(?string $nom): self {
+        $this->nom = $nom;
+        return $this;
+    }
+}
