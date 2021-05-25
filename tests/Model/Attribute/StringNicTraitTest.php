48390848c7f638af1c8cd54b84434f718f94700e
webeweb <support@webeweb.fr>
2021-05-25 19:09:27 +0200

    Add string NIC trait

---
+++ tests/Model/Attribute/StringNicTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model\Attribute;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Model\Attribute\TestStringNicTrait;
+
+/**
+ * String NIC trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model\Attribute
+ */
+class StringNicTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setNic() method().
+     *
+     * @return void
+     */
+    public function testSetNic(): void {
+
+        $obj = new TestStringNicTrait();
+
+        $obj->setNic("nic");
+        $this->assertEquals("nic", $obj->getNic());
+    }
+}
