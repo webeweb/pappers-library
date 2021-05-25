b419654084c41e384845662b78521f2c225796b2
webeweb <support@webeweb.fr>
2021-05-25 19:10:52 +0200

    Add string SIRET trait

---
+++ tests/Fixtures/Model/Attribute/TestStringSiretTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Fixtures\Model\Attribute;
+
+use WBW\Library\Pappers\Model\Attribute\StringSiretTrait;
+
+/**
+ * Test string SIRET trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Fixtures\Model\Attribute
+ */
+class TestStringSiretTrait {
+
+    use StringSiretTrait;
+}
