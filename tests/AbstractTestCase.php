943c239575e3e9ac3810cbd8688db085fb034481
webeweb <support@webeweb.fr>
2021-05-25 19:16:34 +0200

    Add abstract Test case

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * Abstract test case.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests
+ * @abstract
+ */
+abstract class AbstractTestCase extends BaseTestCase {
+
+}
