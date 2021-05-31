dacb8b88acfd4077d9bf3c5c6279bd9f056980bc
webeweb <support@webeweb.fr>
2021-05-31 19:26:48 +0200

    Add string code postal trait

---
+++ src/Model/Attribute/StringCodePostalTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model\Attribute;
+
+/**
+ * String Code postal trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model\Attribute
+ */
+trait StringCodePostalTrait {
+
+    /**
+     * Code postal.
+     *
+     * @var string|null
+     */
+    private $codePostal;
+
+    /**
+     * Get the code postal.
+     *
+     * @return string|null Returns the code postal.
+     */
+    public function getCodePostal(): ?string {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set the code postal.
+     *
+     * @param string|null $codePostal The code postal.
+     * @return self Returns this instance.
+     */
+    public function setCodePostal(?string $codePostal): self {
+        $this->codePostal = $codePostal;
+        return $this;
+    }
+}
