ba42c8a0de3d251e00e0a8992e25bfb65dcd6ee4
webeweb <support@webeweb.fr>
2021-05-31 19:26:01 +0200

    Add integer pourcentage pleine propriété trait

---
+++ src/Model/Attribute/IntegerPourcentagePleineProprieteTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model\Attribute;
+
+/**
+ * Integer Pourcentage pleine propriété.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model\Attribute
+ */
+trait IntegerPourcentagePleineProprieteTrait {
+
+    /**
+     * Pourcentage pleine propriété.
+     *
+     * @var int|null
+     */
+    private $pourcentagePleinePropriete;
+
+    /**
+     * Get the pourcentage pleine propriété.
+     *
+     * @return int|null Returns the pourcentage pleine propriété.
+     */
+    public function getPourcentagePleinePropriete(): ?int {
+        return $this->pourcentagePleinePropriete;
+    }
+
+    /**
+     * Set the pourcentage pleine propriété.
+     *
+     * @param int|null $pourcentagePleinePropriete The pourcentage pleine propriété.
+     * @return self Returns this instance.
+     */
+    public function setPourcentagePleinePropriete(?int $pourcentagePleinePropriete): self {
+        $this->pourcentagePleinePropriete = $pourcentagePleinePropriete;
+        return $this;
+    }
+}
---
+++ tests/Model/Attribute/IntegerPourcentagePleineProprieteTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model\Attribute;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Model\Attribute\TestIntegerPourcentagePleineProprieteTrait;
+
+/**
+ * Integer Pourcentage pleine propriété trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model\Attribute
+ */
+class IntegerPourcentagePleineProprieteTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setPourcentagePleinePropriete() method().
+     *
+     * @return void
+     */
+    public function testSetPourcentagePleinePropriete(): void {
+
+        $obj = new TestIntegerPourcentagePleineProprieteTrait();
+
+        $obj->setPourcentagePleinePropriete(1);
+        $this->assertEquals(1, $obj->getPourcentagePleinePropriete());
+    }
+}
