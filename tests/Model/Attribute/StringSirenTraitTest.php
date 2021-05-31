fcd8ada9717237f9832af0f06eeff5d3da323110
webeweb <support@webeweb.fr>
2021-05-31 19:27:09 +0200

    Add string SIREN trait

---
+++ tests/Model/Attribute/StringSirenTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model\Attribute;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Model\Attribute\TestStringSirenTrait;
+
+/**
+ * String SIREN trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model\Attribute
+ */
+class StringSirenTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setSiren() method().
+     *
+     * @return void
+     */
+    public function testSetSiren(): void {
+
+        $obj = new TestStringSirenTrait();
+
+        $obj->setSiren("siren");
+        $this->assertEquals("siren", $obj->getSiren());
+    }
+}
