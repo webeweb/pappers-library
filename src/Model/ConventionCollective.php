516ca18065ab23345ea9825d6f3ba81d987c2fa5
webeweb <support@webeweb.fr>
2021-06-01 13:23:47 +0200

    Add Convention collective model

---
+++ src/Model/ConventionCollective.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Pappers\Model\Attribute\StringNomTrait;
+
+/**
+ * Convention collective.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class ConventionCollective {
+
+    use StringNomTrait;
+
+    /**
+     * Confirmée.
+     *
+     * @var bool|null
+     */
+    private $confirmee;
+
+    /**
+     * IDCC.
+     *
+     * @var int|null
+     */
+    private $idcc;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the confirmée.
+     *
+     * @return bool|null Returns the confirmée.
+     */
+    public function getConfirmee(): ?bool {
+        return $this->confirmee;
+    }
+
+    /**
+     * Get the IDCC.
+     *
+     * @return int|null Returns the IDCC.
+     */
+    public function getIdcc(): ?int {
+        return $this->idcc;
+    }
+
+    /**
+     * Set the confirmée.
+     *
+     * @param bool|null $confirmee The confirmée.
+     * @return ConventionCollective Returns this convention collective.
+     */
+    public function setConfirmee(?bool $confirmee): ConventionCollective {
+        $this->confirmee = $confirmee;
+        return $this;
+    }
+
+    /**
+     * Set the IDCC.
+     *
+     * @param int|null $idcc The IDCC.
+     * @return ConventionCollective Returns this convention collective.
+     */
+    public function setIdcc(?int $idcc): ConventionCollective {
+        $this->idcc = $idcc;
+        return $this;
+    }
+}
---
+++ tests/Model/ConventionCollectiveTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\ConventionCollective;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Convention collective test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class ConventionCollectiveTest extends AbstractTestCase {
+
+    /**
+     * Tests the setConfirmee() method.
+     *
+     * @return void
+     */
+    public function testSetConfirmee(): void {
+
+        $obj = new ConventionCollective();
+
+        $obj->setConfirmee(true);
+        $this->assertTrue($obj->getConfirmee());
+    }
+
+    /**
+     * Tests the setIdcc() method.
+     *
+     * @return void
+     */
+    public function testSetIdcc(): void {
+
+        $obj = new ConventionCollective();
+
+        $obj->setIdcc(1);
+        $this->assertEquals(1, $obj->getIdcc());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new ConventionCollective();
+
+        $this->assertNull($obj->getNom());
+
+        $this->assertNull($obj->getConfirmee());
+        $this->assertNull($obj->getIdcc());
+    }
+}
