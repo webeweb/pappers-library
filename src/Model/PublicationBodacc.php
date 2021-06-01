a460b170be4e1ad4d75cccf32b33c36ae7989399
webeweb <support@webeweb.fr>
2021-06-01 13:17:20 +0200

    Add Publication BODACC model

---
+++ src/Model/PublicationBodacc.php
@@ -0,0 +1,363 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Core\Model\Attribute\StringTypeTrait;
+use WBW\Library\Pappers\Model\Attribute\StringNomTrait;
+use WBW\Library\Pappers\Model\Attribute\StringPersonneMoraleTrait;
+use WBW\Library\Pappers\Model\Attribute\StringPrenomTrait;
+
+/**
+ * Publication BODACC.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class PublicationBodacc {
+
+    use StringNomTrait;
+    use StringPersonneMoraleTrait;
+    use StringPrenomTrait;
+    use StringTypeTrait;
+
+    /**
+     * Activité.
+     *
+     * @var string|null
+     */
+    private $activite;
+
+    /**
+     * Administration.
+     *
+     * @var string|null
+     */
+    private $administration;
+
+    /**
+     * Adresse.
+     *
+     * @var string|null
+     */
+    private $adresse;
+
+    /**
+     * BODACC.
+     *
+     * @var string|null
+     */
+    private $bodacc;
+
+    /**
+     * Capital.
+     *
+     * @var int|null
+     */
+    private $capital;
+
+    /**
+     * Date.
+     *
+     * @var string|null
+     */
+    private $date;
+
+    /**
+     * Date début activité.
+     *
+     * @var string|null
+     */
+    private $dateDebutActivite;
+
+    /**
+     * Dénomination.
+     *
+     * @var string|null
+     */
+    private $denomination;
+
+    /**
+     * Nom entreprise.
+     *
+     * @var string|null
+     */
+    private $nomEntreprise;
+
+    /**
+     * Numéro annonce.
+     *
+     * @var string|null
+     */
+    private $numeroAnnonce;
+
+    /**
+     * Numéro parution.
+     *
+     * @var string|null
+     */
+    private $numeroParution;
+
+
+    /**
+     * RCS.
+     *
+     * @var string|null
+     */
+    private $rcs;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the activité.
+     *
+     * @return string|null Returns the activité.
+     */
+    public function getActivite(): ?string {
+        return $this->activite;
+    }
+
+    /**
+     * Get the administration.
+     *
+     * @return string|null Returns the administration.
+     */
+    public function getAdministration(): ?string {
+        return $this->administration;
+    }
+
+    /**
+     * Get the adresse.
+     *
+     * @return string|null Returns the adresse.
+     */
+    public function getAdresse(): ?string {
+        return $this->adresse;
+    }
+
+    /**
+     * Get the BODACC.
+     *
+     * @return string|null Returns the BODACC.
+     */
+    public function getBodacc(): ?string {
+        return $this->bodacc;
+    }
+
+    /**
+     * Get the capital.
+     *
+     * @return int|null Returns the capital.
+     */
+    public function getCapital(): ?int {
+        return $this->capital;
+    }
+
+    /**
+     * Get the date.
+     *
+     * @return string|null Returns the date.
+     */
+    public function getDate(): ?string {
+        return $this->date;
+    }
+
+    /**
+     * Get the date début activité.
+     *
+     * @return string|null Returns the date début activité.
+     */
+    public function getDateDebutActivite(): ?string {
+        return $this->dateDebutActivite;
+    }
+
+    /**
+     * Get the dénomination.
+     *
+     * @return string|null Returns the dénomination.
+     */
+    public function getDenomination(): ?string {
+        return $this->denomination;
+    }
+
+    /**
+     * Get the nom entreprise.
+     *
+     * @return string|null Returns the nom entreprise.
+     */
+    public function getNomEntreprise(): ?string {
+        return $this->nomEntreprise;
+    }
+
+    /**
+     * Get the numéro annonce.
+     *
+     * @return string|null Returns the numéro annonce.
+     */
+    public function getNumeroAnnonce(): ?string {
+        return $this->numeroAnnonce;
+    }
+
+    /**
+     * Get the numéro parution.
+     *
+     * @return string|null Returns the numéro parution.
+     */
+    public function getNumeroParution(): ?string {
+        return $this->numeroParution;
+    }
+
+    /**
+     * Get the RCS.
+     *
+     * @return string|null Returns the RCS.
+     */
+    public function getRcs(): ?string {
+        return $this->rcs;
+    }
+
+    /**
+     * Set the activité.
+     *
+     * @param string|null $activite The activité.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setActivite(?string $activite): PublicationBodacc {
+        $this->activite = $activite;
+        return $this;
+    }
+
+    /**
+     * Set the administration.
+     *
+     * @param string|null $administration The administration.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setAdministration(?string $administration): PublicationBodacc {
+        $this->administration = $administration;
+        return $this;
+    }
+
+    /**
+     * Set the adresse.
+     *
+     * @param string|null $adresse The adresse.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setAdresse(?string $adresse): PublicationBodacc {
+        $this->adresse = $adresse;
+        return $this;
+    }
+
+    /**
+     * Set the BODACC.
+     *
+     * @param string|null $bodacc The BODACC.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setBodacc(?string $bodacc): PublicationBodacc {
+        $this->bodacc = $bodacc;
+        return $this;
+    }
+
+    /**
+     * Set the capital.
+     *
+     * @param int|null $capital The capital.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setCapital(?int $capital): PublicationBodacc {
+        $this->capital = $capital;
+        return $this;
+    }
+
+    /**
+     * Set the date.
+     *
+     * @param string|null $date The date.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setDate(?string $date): PublicationBodacc {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * Set the date début activité.
+     *
+     * @param string|null $dateDebutActivite The date aébut activité.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setDateDebutActivite(?string $dateDebutActivite): PublicationBodacc {
+        $this->dateDebutActivite = $dateDebutActivite;
+        return $this;
+    }
+
+    /**
+     * Set the dénomination.
+     *
+     * @param string|null $denomination The dénomination.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setDenomination(?string $denomination): PublicationBodacc {
+        $this->denomination = $denomination;
+        return $this;
+    }
+
+    /**
+     * Set the nom entreprise.
+     *
+     * @param string|null $nomEntreprise The nom entreprise.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setNomEntreprise(?string $nomEntreprise): PublicationBodacc {
+        $this->nomEntreprise = $nomEntreprise;
+        return $this;
+    }
+
+    /**
+     * Set the numéro annonce.
+     *
+     * @param string|null $numeroAnnonce The numéro annonce.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setNumeroAnnonce(?string $numeroAnnonce): PublicationBodacc {
+        $this->numeroAnnonce = $numeroAnnonce;
+        return $this;
+    }
+
+    /**
+     * Set the numéro parution.
+     *
+     * @param string|null $numeroParution The numéro parution.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setNumeroParution(?string $numeroParution): PublicationBodacc {
+        $this->numeroParution = $numeroParution;
+        return $this;
+    }
+
+    /**
+     * Set the RCS.
+     *
+     * @param string|null $rcs The RCS.
+     * @return PublicationBodacc Returns this publication BODACC.
+     */
+    public function setRcs(?string $rcs): PublicationBodacc {
+        $this->rcs = $rcs;
+        return $this;
+    }
+}
---
+++ tests/Model/PublicationBodaccTest.php
@@ -0,0 +1,208 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\PublicationBodacc;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Publication BODACC test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class PublicationBodaccTest extends AbstractTestCase {
+
+    /**
+     * Tests the setActivite() method.
+     *
+     * @return void
+     */
+    public function testSetActivite(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setActivite("activite");
+        $this->assertEquals("activite", $obj->getActivite());
+    }
+
+    /**
+     * Tests the set() method.
+     *
+     * @return void
+     */
+    public function testSetAdministration(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setAdministration("administration");
+        $this->assertEquals("administration", $obj->getAdministration());
+    }
+
+    /**
+     * Tests the setAdresse() method.
+     *
+     * @return void
+     */
+    public function testSetAdresse(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setAdresse("adresse");
+        $this->assertEquals("adresse", $obj->getAdresse());
+    }
+
+    /**
+     * Tests the setBodacc() method.
+     *
+     * @return void
+     */
+    public function testSetBodacc(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setBodacc("bodacc");
+        $this->assertEquals("bodacc", $obj->getBodacc());
+    }
+
+    /**
+     * Tests the setCapital() method.
+     *
+     * @return void
+     */
+    public function testSetCapital(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setCapital(1);
+        $this->assertEquals(1, $obj->getCapital());
+    }
+
+    /**
+     * Tests the setDate() method.
+     *
+     * @return void
+     */
+    public function testSetDate(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setDate("date");
+        $this->assertEquals("date", $obj->getDate());
+    }
+
+    /**
+     * Tests the setDateDebutActivite() method.
+     *
+     * @return void
+     */
+    public function testSetDateDebutActivite(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setDateDebutActivite("dateDebutActivite");
+        $this->assertEquals("dateDebutActivite", $obj->getDateDebutActivite());
+    }
+
+    /**
+     * Tests the setDenomination() method.
+     *
+     * @return void
+     */
+    public function testSetDenomination(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setDenomination("denomination");
+        $this->assertEquals("denomination", $obj->getDenomination());
+    }
+
+    /**
+     * Tests the setNomEntreprise() method.
+     *
+     * @return void
+     */
+    public function testSetNomEntreprise(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setNomEntreprise("nomEntreprise");
+        $this->assertEquals("nomEntreprise", $obj->getNomEntreprise());
+    }
+
+    /**
+     * Tests the setNumeroAnnonce() method.
+     *
+     * @return void
+     */
+    public function testSetNumeroAnnonce(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setNumeroAnnonce("numeroAnnonce");
+        $this->assertEquals("numeroAnnonce", $obj->getNumeroAnnonce());
+    }
+
+    /**
+     * Tests the setNumeroParution() method.
+     *
+     * @return void
+     */
+    public function testSetNumeroParution(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setNumeroParution("numeroParution");
+        $this->assertEquals("numeroParution", $obj->getNumeroParution());
+    }
+
+    /**
+     * Tests the setRcs() method.
+     *
+     * @return void
+     */
+    public function testSetRcs(): void {
+
+        $obj = new PublicationBodacc();
+
+        $obj->setRcs("rcs");
+        $this->assertEquals("rcs", $obj->getRcs());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new PublicationBodacc();
+
+        $this->assertNull($obj->getNom());
+        $this->assertNull($obj->getPersonneMorale());
+        $this->assertNull($obj->getPrenom());
+        $this->assertNull($obj->getType());
+
+        $this->assertNull($obj->getActivite());
+        $this->assertNull($obj->getAdministration());
+        $this->assertNull($obj->getAdresse());
+        $this->assertNull($obj->getBodacc());
+        $this->assertNull($obj->getCapital());
+        $this->assertNull($obj->getDate());
+        $this->assertNull($obj->getDateDebutActivite());
+        $this->assertNull($obj->getDenomination());
+        $this->assertNull($obj->getNomEntreprise());
+        $this->assertNull($obj->getNumeroAnnonce());
+        $this->assertNull($obj->getNumeroParution());
+        $this->assertNull($obj->getRcs());
+    }
+}
