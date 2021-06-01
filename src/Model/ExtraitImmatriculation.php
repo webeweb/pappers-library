3213fd6aa7e574e01c84e112b78ff0fdb920cca2
webeweb <support@webeweb.fr>
2021-06-01 13:20:07 +0200

    Add Extrait immatriculation model

---
+++ src/Model/ExtraitImmatriculation.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Pappers\Model\Attribute\StringTokenTrait;
+
+/**
+ * Extrait immatriculation.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class ExtraitImmatriculation {
+
+    use StringTokenTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
---
+++ tests/Model/ExtraitImmatriculationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\ExtraitImmatriculation;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Extrait immatriculation test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class ExtraitImmatriculationTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct() {
+
+        $obj = new ExtraitImmatriculation();
+
+        $this->assertNull($obj->getToken());
+    }
+}
