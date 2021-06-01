5a2989250aa05ec8c59ccfdcdcd55687ea863e52
webeweb <support@webeweb.fr>
2021-06-01 13:25:36 +0200

    Add Dépôt acte model

---
+++ src/Model/DepotActe.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Pappers\Model\Attribute\BooleanDisponibleTrait;
+use WBW\Library\Pappers\Model\Attribute\StringDateDepotFormateTrait;
+use WBW\Library\Pappers\Model\Attribute\StringDateDepotTrait;
+use WBW\Library\Pappers\Model\Attribute\StringNomFichierPdfTrait;
+use WBW\Library\Pappers\Model\Attribute\StringTokenTrait;
+
+/**
+ * Dépôt acte.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class DepotActe {
+
+    use BooleanDisponibleTrait;
+    use StringDateDepotTrait;
+    use StringDateDepotFormateTrait;
+    use StringNomFichierPdfTrait;
+    use StringTokenTrait;
+
+    /**
+     * Actes.
+     *
+     * @var Acte[]
+     */
+    private $actes;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->setActes([]);
+    }
+
+    /**
+     * Add an acte.
+     *
+     * @param Acte $acte The acte.
+     * @return DepotActe Returns this dépôt.
+     */
+    public function addActe(Acte $acte): DepotActe {
+        $this->actes[] = $acte;
+        return $this;
+    }
+
+    /**
+     * Get the actes.
+     *
+     * @return Acte[] Returns the actes.
+     */
+    public function getActes(): array {
+        return $this->actes;
+    }
+
+    /**
+     * Set the actes.
+     *
+     * @param Acte[] $actes The actes.
+     * @return DepotActe Returns this dépôt.
+     */
+    protected function setActes(array $actes): DepotActe {
+        $this->actes = $actes;
+        return $this;
+    }
+
+}
---
+++ tests/Model/DepotActeTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\Acte;
+use WBW\Library\Pappers\Model\DepotActe;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Dépôt acte test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class DepotActeTest extends AbstractTestCase {
+
+    /**
+     * Tests the addActe() method.
+     *
+     * @return void
+     */
+    public function testAddActe(): void {
+
+        // Set an Acte mock.
+        $acte = new Acte();
+
+        $obj = new DepotActe();
+
+        $obj->addActe($acte);
+        $this->assertSame($acte, $obj->getActes()[0]);
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DepotActe();
+
+        $this->assertNull($obj->getDateDepot());
+        $this->assertNull($obj->getDateDepotFormate());
+        $this->assertNull($obj->getDisponible());
+        $this->assertNull($obj->getNomFichierPdf());
+        $this->assertNull($obj->getToken());
+
+        $this->assertEquals([], $obj->getActes());
+    }
+}
