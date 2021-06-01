0399ccfcc5de159fd05220d06a817af9ca610b22
webeweb <support@webeweb.fr>
2021-06-01 13:22:36 +0200

    Add Représentant model

---
+++ src/Model/Representant.php
@@ -0,0 +1,291 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Pappers\Model\Attribute\StringAdresseLigne1Trait;
+use WBW\Library\Pappers\Model\Attribute\StringAdresseLigne2Trait;
+use WBW\Library\Pappers\Model\Attribute\StringCodePostalTrait;
+use WBW\Library\Pappers\Model\Attribute\StringDateNaissanceFormateTrait;
+use WBW\Library\Pappers\Model\Attribute\StringNationaliteTrait;
+use WBW\Library\Pappers\Model\Attribute\StringNomTrait;
+use WBW\Library\Pappers\Model\Attribute\StringPersonneMoraleTrait;
+use WBW\Library\Pappers\Model\Attribute\StringPrenomTrait;
+use WBW\Library\Pappers\Model\Attribute\StringVilleTrait;
+
+/**
+ * Représentant.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class Representant {
+
+    use StringAdresseLigne1Trait;
+    use StringAdresseLigne2Trait;
+    use StringCodePostalTrait;
+    use StringDateNaissanceFormateTrait;
+    use StringNationaliteTrait;
+    use StringNomTrait;
+    use StringPersonneMoraleTrait;
+    use StringPrenomTrait;
+    use StringVilleTrait;
+
+    /**
+     * Adresse ligne 3.
+     *
+     * @var string|null
+     */
+    private $adresseLigne3;
+
+    /**
+     * Age.
+     *
+     * @var int|null
+     */
+    private $age;
+
+    /**
+     * Date naissance.
+     *
+     * @var string|null
+     */
+    private $dateNaissance;
+
+    /**
+     * Date prise poste.
+     *
+     * @var string|null
+     */
+    private $datePrisePoste;
+
+    /**
+     * Nom complet.
+     *
+     * @var string|null
+     */
+    private $nomComplet;
+
+    /**
+     * Pays.
+     *
+     * @var string|null
+     */
+    private $pays;
+
+    /**
+     * Pays naissance.
+     *
+     * @var string|null
+     */
+    private $paysNaissance;
+
+    /**
+     * Qualité.
+     *
+     * @var string|null
+     */
+    private $qualite;
+
+    /**
+     * Ville naissance.
+     *
+     * @var string|null
+     */
+    private $villeNaissance;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the adresse ligne 3.
+     *
+     * @return string|null Returns the adresse ligne 3.
+     */
+    public function getAdresseLigne3(): ?string {
+        return $this->adresseLigne3;
+    }
+
+    /**
+     * Get the age.
+     *
+     * @return int|null Returns the age.
+     */
+    public function getAge(): ?int {
+        return $this->age;
+    }
+
+    /**
+     * Get the date naissance.
+     *
+     * @return string|null Returns the date naissance.
+     */
+    public function getDateNaissance(): ?string {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * Get the date prise poste.
+     *
+     * @return string|null Returns the date prise poste.
+     */
+    public function getDatePrisePoste(): ?string {
+        return $this->datePrisePoste;
+    }
+
+    /**
+     * Get the nom complet.
+     *
+     * @return string|null Returns the nom complet.
+     */
+    public function getNomComplet(): ?string {
+        return $this->nomComplet;
+    }
+
+    /**
+     * Get the pays.
+     *
+     * @return string|null Returns the pays.
+     */
+    public function getPays(): ?string {
+        return $this->pays;
+    }
+
+    /**
+     * Get the pays naissance.
+     *
+     * @return string|null Returns the pays naissance.
+     */
+    public function getPaysNaissance(): ?string {
+        return $this->paysNaissance;
+    }
+
+    /**
+     * Get the qualité.
+     *
+     * @return string|null Returns the qualité.
+     */
+    public function getQualite(): ?string {
+        return $this->qualite;
+    }
+
+    /**
+     * Get the ville naissance.
+     *
+     * @return string|null Returns the ville naissance.
+     */
+    public function getVilleNaissance(): ?string {
+        return $this->villeNaissance;
+    }
+
+    /**
+     * Set the adresse ligne 3.
+     *
+     * @param string|null $adresseLigne3 The adresse ligne 3.
+     * @return Representant Returns this représentant.
+     */
+    public function setAdresseLigne3(?string $adresseLigne3): Representant {
+        $this->adresseLigne3 = $adresseLigne3;
+        return $this;
+    }
+
+    /**
+     * Set the age.
+     *
+     * @param int|null $age The age.
+     * @return Representant Returns this représentant.
+     */
+    public function setAge(?int $age): Representant {
+        $this->age = $age;
+        return $this;
+    }
+
+    /**
+     * Set the date naissance.
+     *
+     * @param string|null $dateNaissance The date naissance.
+     * @return Representant Returns this représentant.
+     */
+    public function setDateNaissance(?string $dateNaissance): Representant {
+        $this->dateNaissance = $dateNaissance;
+        return $this;
+    }
+
+    /**
+     * Set the date prise poste.
+     *
+     * @param string|null $datePrisePoste The date prise poste.
+     * @return Representant Returns this représentant.
+     */
+    public function setDatePrisePoste(?string $datePrisePoste): Representant {
+        $this->datePrisePoste = $datePrisePoste;
+        return $this;
+    }
+
+    /**
+     * Set the nom complet.
+     *
+     * @param string|null $nomComplet The nom complet.
+     * @return Representant Returns this représentant.
+     */
+    public function setNomComplet(?string $nomComplet): Representant {
+        $this->nomComplet = $nomComplet;
+        return $this;
+    }
+
+    /**
+     * Set the pays.
+     *
+     * @param string|null $pays The pays.
+     * @return Representant Returns this représentant.
+     */
+    public function setPays(?string $pays): Representant {
+        $this->pays = $pays;
+        return $this;
+    }
+
+    /**
+     * Set the pays naissance.
+     *
+     * @param string|null $paysNaissance The pays naissance.
+     * @return Representant Returns this représentant.
+     */
+    public function setPaysNaissance(?string $paysNaissance): Representant {
+        $this->paysNaissance = $paysNaissance;
+        return $this;
+    }
+
+    /**
+     * Set the qualité.
+     *
+     * @param string|null $qualite The qualité.
+     * @return Representant Returns this représentant.
+     */
+    public function setQualite(?string $qualite): Representant {
+        $this->qualite = $qualite;
+        return $this;
+    }
+
+    /**
+     * Set the ville naissance.
+     *
+     * @param string|null $villeNaissance The ville naissance.
+     * @return Representant Returns this représentant.
+     */
+    public function setVilleNaissance(?string $villeNaissance): Representant {
+        $this->villeNaissance = $villeNaissance;
+        return $this;
+    }
+}
---
+++ tests/Model/RepresentantTest.php
@@ -0,0 +1,171 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\Representant;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Représentant test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class RepresentantTest extends AbstractTestCase {
+
+    /**
+     * Tests the setAdresseLigne3() method.
+     *
+     * @return void
+     */
+    public function testSetAdresseLigne3(): void {
+
+        $obj = new Representant();
+
+        $obj->setAdresseLigne3("adresseLigne3");
+        $this->assertEquals("adresseLigne3", $obj->getAdresseLigne3());
+    }
+
+    /**
+     * Tests the setAge() method.
+     *
+     * @return void
+     */
+    public function testSetAge(): void {
+
+        $obj = new Representant();
+
+        $obj->setAge(1);
+        $this->assertEquals(1, $obj->getAge());
+    }
+
+    /**
+     * Tests the setDateNaissance() method.
+     *
+     * @return void
+     */
+    public function testSetDateNaissance(): void {
+
+        $obj = new Representant();
+
+        $obj->setDateNaissance("dateNaissance");
+        $this->assertEquals("dateNaissance", $obj->getDateNaissance());
+    }
+
+    /**
+     * Tests the setDatePrisePoste() method.
+     *
+     * @return void
+     */
+    public function testSetDatePrisePoste(): void {
+
+        $obj = new Representant();
+
+        $obj->setDatePrisePoste("datePrisePoste");
+        $this->assertEquals("datePrisePoste", $obj->getDatePrisePoste());
+    }
+
+    /**
+     * Tests the setNomComplet() method.
+     *
+     * @return void
+     */
+    public function testSetNomComplet(): void {
+
+        $obj = new Representant();
+
+        $obj->setNomComplet("nomComplet");
+        $this->assertEquals("nomComplet", $obj->getNomComplet());
+    }
+
+    /**
+     * Tests the setPays() method.
+     *
+     * @return void
+     */
+    public function testSetPays(): void {
+
+        $obj = new Representant();
+
+        $obj->setPays("nom");
+        $this->assertEquals("nom", $obj->getPays());
+    }
+
+    /**
+     * Tests the setPaysNaissance() method.
+     *
+     * @return void
+     */
+    public function testSetPaysNaissance(): void {
+
+        $obj = new Representant();
+
+        $obj->setPaysNaissance("pays Naissance");
+        $this->assertEquals("pays Naissance", $obj->getPaysNaissance());
+    }
+
+    /**
+     * Tests the setQualite() method.
+     *
+     * @return void
+     */
+    public function testSetQualite(): void {
+
+        $obj = new Representant();
+
+        $obj->setQualite("qualite");
+        $this->assertEquals("qualite", $obj->getQualite());
+    }
+
+    /**
+     * Tests the setVilleNaissance() method.
+     *
+     * @return void
+     */
+    public function testSetVilleNaissance(): void {
+
+        $obj = new Representant();
+
+        $obj->setVilleNaissance("villeNaissance");
+        $this->assertEquals("villeNaissance", $obj->getVilleNaissance());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Representant();
+
+        $this->assertNull($obj->getAdresseLigne1());
+        $this->assertNull($obj->getAdresseLigne2());
+        $this->assertNull($obj->getCodePostal());
+        $this->assertNull($obj->getDateNaissanceFormate());
+        $this->assertNull($obj->getNationalite());
+        $this->assertNull($obj->getNom());
+        $this->assertNull($obj->getPersonneMorale());
+        $this->assertNull($obj->getPrenom());
+        $this->assertNull($obj->getVille());
+
+        $this->assertNull($obj->getQualite());
+        $this->assertNull($obj->getDatePrisePoste());
+        $this->assertNull($obj->getNomComplet());
+        $this->assertNull($obj->getDateNaissance());
+        $this->assertNull($obj->getAge());
+        $this->assertNull($obj->getVilleNaissance());
+        $this->assertNull($obj->getPaysNaissance());
+        $this->assertNull($obj->getAdresseLigne3());
+        $this->assertNull($obj->getPays());
+    }
+}
