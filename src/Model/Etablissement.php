c1e0c7c73ee5def635d444f2455c7e3dfffd08ea
webeweb <support@webeweb.fr>
2021-06-01 13:23:14 +0200

    Add Etablissement model

---
+++ src/Model/Etablissement.php
@@ -0,0 +1,405 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Pappers\Model\Attribute\IntegerAnneeEffectifTrait;
+use WBW\Library\Pappers\Model\Attribute\IntegerEffectifMaxTrait;
+use WBW\Library\Pappers\Model\Attribute\IntegerEffectifMinTrait;
+use WBW\Library\Pappers\Model\Attribute\StringAdresseLigne1Trait;
+use WBW\Library\Pappers\Model\Attribute\StringAdresseLigne2Trait;
+use WBW\Library\Pappers\Model\Attribute\StringCodePostalTrait;
+use WBW\Library\Pappers\Model\Attribute\StringEffectifTrait;
+use WBW\Library\Pappers\Model\Attribute\StringNicTrait;
+use WBW\Library\Pappers\Model\Attribute\StringSiretFormateTrait;
+use WBW\Library\Pappers\Model\Attribute\StringSiretTrait;
+use WBW\Library\Pappers\Model\Attribute\StringTrancheEffectifTrait;
+use WBW\Library\Pappers\Model\Attribute\StringVilleTrait;
+
+/**
+ * Etablissement.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class Etablissement {
+
+    use IntegerAnneeEffectifTrait;
+    use IntegerEffectifMaxTrait;
+    use IntegerEffectifMinTrait;
+    use StringAdresseLigne1Trait;
+    use StringAdresseLigne2Trait;
+    use StringCodePostalTrait;
+    use StringEffectifTrait;
+    use StringNicTrait;
+    use StringSiretFormateTrait;
+    use StringSiretTrait;
+    use StringTrancheEffectifTrait;
+    use StringVilleTrait;
+
+    /**
+     * Code NAF.
+     *
+     * @var string|null
+     */
+    private $codeNaf;
+
+    /**
+     * Complément adresse.
+     *
+     * @var string|null
+     */
+    private $complementAdresse;
+
+    /**
+     * Date création.
+     *
+     * @var string|null
+     */
+    private $dateCreation;
+
+    /**
+     * Etablissement cessé.
+     *
+     * @var bool|null
+     */
+    private $etablissementCesse;
+
+    /**
+     * Etablissment employeur.
+     *
+     * @var bool|null
+     */
+    private $etablissementEmployeur;
+
+    /**
+     * Indice répétition.
+     *
+     * @var string|null
+     */
+    private $indiceRepetition;
+
+    /**
+     * Latitude.
+     *
+     * @var float|null
+     */
+    private $latitude;
+
+    /**
+     * Libellé code NAF.
+     *
+     * @var string|null
+     */
+    private $libelleCodeNaf;
+
+    /**
+     * Libellé voie.
+     *
+     * @var string|null
+     */
+    private $libelleVoie;
+
+    /**
+     * Longitude.
+     *
+     * @var float|null
+     */
+    private $longitude;
+
+    /**
+     * Numéro voie.
+     *
+     * @var int|null
+     */
+    private $numeroVoie;
+
+    /**
+     * Siège.
+     *
+     * @var bool|null
+     */
+    private $siege;
+
+    /**
+     * Type voie.
+     *
+     * @var string|null
+     */
+    private $typeVoie;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+
+    /**
+     * Get the code NAF.
+     *
+     * @return string|null Returns the code NAF.
+     */
+    public function getCodeNaf(): ?string {
+        return $this->codeNaf;
+    }
+
+    /**
+     * Get the complément adresse.
+     *
+     * @return string|null Returns the complément adresse.
+     */
+    public function getComplementAdresse(): ?string {
+        return $this->complementAdresse;
+    }
+
+    /**
+     * Get the date création.
+     *
+     * @return string|null Returns the date création.
+     */
+    public function getDateCreation(): ?string {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Get the établissement cessé.
+     *
+     * @return bool|null Returns the établissement cessé.
+     */
+    public function getEtablissementCesse(): ?bool {
+        return $this->etablissementCesse;
+    }
+
+    /**
+     * Get the établissement employeur.
+     *
+     * @return bool|null Returns the établissement employeur.
+     */
+    public function getEtablissementEmployeur(): ?bool {
+        return $this->etablissementEmployeur;
+    }
+
+    /**
+     * Get the indice répétition.
+     *
+     * @return string|null Returns the indice répétition.
+     */
+    public function getIndiceRepetition(): ?string {
+        return $this->indiceRepetition;
+    }
+
+    /**
+     * Get the latitude.
+     *
+     * @return float|null Returns the latitude.
+     */
+    public function getLatitude(): ?float {
+        return $this->latitude;
+    }
+
+    /**
+     * Get the libellé code NAF.
+     *
+     * @return string|null Returns the libellé code NAF.
+     */
+    public function getLibelleCodeNaf(): ?string {
+        return $this->libelleCodeNaf;
+    }
+
+    /**
+     * Get the libellé voie.
+     *
+     * @return string|null Returns the libellé voie.
+     */
+    public function getLibelleVoie(): ?string {
+        return $this->libelleVoie;
+    }
+
+    /**
+     * Get the longitude.
+     *
+     * @return float|null Returns the longitude.
+     */
+    public function getLongitude(): ?float {
+        return $this->longitude;
+    }
+
+    /**
+     * Get the numéro voie.
+     *
+     * @return int|null Returns the numéro voie.
+     */
+    public function getNumeroVoie(): ?int {
+        return $this->numeroVoie;
+    }
+
+    /**
+     * Get the siège.
+     *
+     * @return bool|null Returns the siège.
+     */
+    public function getSiege(): ?bool {
+        return $this->siege;
+    }
+
+    /**
+     * Get the type voie.
+     *
+     * @return string|null Returns the type voie.
+     */
+    public function getTypeVoie(): ?string {
+        return $this->typeVoie;
+    }
+
+    /**
+     * Set the code NAF.
+     *
+     * @param string|null $codeNaf The code NAF.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setCodeNaf(?string $codeNaf): Etablissement {
+        $this->codeNaf = $codeNaf;
+        return $this;
+    }
+
+    /**
+     * Set the complément adresse.
+     *
+     * @param string|null $complementAdresse The complément adresse.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setComplementAdresse(?string $complementAdresse): Etablissement {
+        $this->complementAdresse = $complementAdresse;
+        return $this;
+    }
+
+    /**
+     * Set the date création.
+     *
+     * @param string|null $dateCreation The date création.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setDateCreation(?string $dateCreation): Etablissement {
+        $this->dateCreation = $dateCreation;
+        return $this;
+    }
+
+    /**
+     * Set the établissement cessé.
+     *
+     * @param bool|null $etablissementCesse The établissement cessé.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setEtablissementCesse(?bool $etablissementCesse): Etablissement {
+        $this->etablissementCesse = $etablissementCesse;
+        return $this;
+    }
+
+    /**
+     * Set the établissement employeur.
+     *
+     * @param bool|null $etablissementEmployeur The établissement employeur.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setEtablissementEmployeur(?bool $etablissementEmployeur): Etablissement {
+        $this->etablissementEmployeur = $etablissementEmployeur;
+        return $this;
+    }
+
+    /**
+     * Set the indice répétition.
+     *
+     * @param string|null $indiceRepetition The indice répétition.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setIndiceRepetition(?string $indiceRepetition): Etablissement {
+        $this->indiceRepetition = $indiceRepetition;
+        return $this;
+    }
+
+    /**
+     * Set the latitude.
+     *
+     * @param float|null $latitude The latitude.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setLatitude(?float $latitude): Etablissement {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     * Set the libellé code NAF.
+     *
+     * @param string|null $libelleCodeNaf The libellé code NAF.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setLibelleCodeNaf(?string $libelleCodeNaf): Etablissement {
+        $this->libelleCodeNaf = $libelleCodeNaf;
+        return $this;
+    }
+
+    /**
+     * Set the libellé voie.
+     *
+     * @param string|null $libelleVoie The libellé voie.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setLibelleVoie(?string $libelleVoie): Etablissement {
+        $this->libelleVoie = $libelleVoie;
+        return $this;
+    }
+
+    /**
+     * Set the longitude.
+     *
+     * @param float|null $longitude The longitude.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setLongitude(?float $longitude): Etablissement {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    /**
+     * Set the numéro voie.
+     *
+     * @param int|null $numeroVoie The numéro voie.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setNumeroVoie(?int $numeroVoie): Etablissement {
+        $this->numeroVoie = $numeroVoie;
+        return $this;
+    }
+
+    /**
+     * Set the siège.
+     *
+     * @param bool|null $siege The siège.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setSiege(?bool $siege): Etablissement {
+        $this->siege = $siege;
+        return $this;
+    }
+
+    /**
+     * Set the type voie.
+     *
+     * @param string|null $typeVoie The type voie.
+     * @return Etablissement Returns this établissement.
+     */
+    public function setTypeVoie(?string $typeVoie): Etablissement {
+        $this->typeVoie = $typeVoie;
+        return $this;
+    }
+}
---
+++ tests/Model/EtablissementTest.php
@@ -0,0 +1,230 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\Etablissement;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Etablissement test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class EtablissementTest extends AbstractTestCase {
+
+    /**
+     * Tests the setCodeNaf() method.
+     *
+     * @return void
+     */
+    public function testSetCodeNaf(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setCodeNaf("codeNaf");
+        $this->assertEquals("codeNaf", $obj->getCodeNaf());
+    }
+
+    /**
+     * Tests the setComplementAdresse() method.
+     *
+     * @return void
+     */
+    public function testSetComplementAdresse(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setComplementAdresse("complementAdresse");
+        $this->assertEquals("complementAdresse", $obj->getComplementAdresse());
+    }
+
+    /**
+     * Tests the setDateCreation() method.
+     *
+     * @return void
+     */
+    public function testSetDateCreation(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setDateCreation("dateCreation");
+        $this->assertEquals("dateCreation", $obj->getDateCreation());
+    }
+
+    /**
+     * Tests the setEtablissementCesse() method.
+     *
+     * @return void
+     */
+    public function testSetEtablissementCesse(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setEtablissementCesse(true);
+        $this->assertTrue($obj->getEtablissementCesse());
+    }
+
+    /**
+     * Tests the setEtablissementEmployeur() method.
+     *
+     * @return void
+     */
+    public function testSetEtablissementEmployeur(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setEtablissementEmployeur(true);
+        $this->assertTrue($obj->getEtablissementEmployeur());
+    }
+
+    /**
+     * Tests the setIndiceRepetition() method.
+     *
+     * @return void
+     */
+    public function testSetIndiceRepetition(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setIndiceRepetition("indiceRepetition");
+        $this->assertEquals("indiceRepetition", $obj->getIndiceRepetition());
+    }
+
+    /**
+     * Tests the setLatitude() method.
+     *
+     * @return void
+     */
+    public function testSetLatitude(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setLatitude(0.1);
+        $this->assertEquals(0.1, $obj->getLatitude());
+    }
+
+    /**
+     * Tests the setLibelleCodeNaf() method.
+     *
+     * @return void
+     */
+    public function testSetLibelleCodeNaf(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setLibelleCodeNaf("libelleCodeNaf");
+        $this->assertEquals("libelleCodeNaf", $obj->getLibelleCodeNaf());
+    }
+
+    /**
+     * Tests the setLibelleVoie() method.
+     *
+     * @return void
+     */
+    public function testSetLibelleVoie(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setLibelleVoie("libelleVoie");
+        $this->assertEquals("libelleVoie", $obj->getLibelleVoie());
+    }
+
+    /**
+     * Tests the setLongitude() method.
+     *
+     * @return void
+     */
+    public function testSetLongitude(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setLongitude(0.1);
+        $this->assertEquals(0.1, $obj->getLongitude());
+    }
+
+    /**
+     * Tests the setNumeroVoie() method.
+     *
+     * @return void
+     */
+    public function testSetNumeroVoie(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setNumeroVoie(1);
+        $this->assertEquals(1, $obj->getNumeroVoie());
+    }
+
+    /**
+     * Tests the setSiege() method.
+     *
+     * @return void
+     */
+    public function testSetSiege(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setSiege(true);
+        $this->assertTrue($obj->getSiege());
+    }
+
+    /**
+     * Tests the setTypeVoie() method.
+     *
+     * @return void
+     */
+    public function testSetTypeVoie(): void {
+
+        $obj = new Etablissement();
+
+        $obj->setTypeVoie("typeVoie");
+        $this->assertEquals("typeVoie", $obj->getTypeVoie());
+    }
+
+    /**
+     * Tests the set() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Etablissement();
+
+        $this->assertNull($obj->getAdresseLigne1());
+        $this->assertNull($obj->getAdresseLigne2());
+        $this->assertNull($obj->getAnneeEffectif());
+        $this->assertNull($obj->getCodePostal());
+        $this->assertNull($obj->getEffectif());
+        $this->assertNull($obj->getEffectifMax());
+        $this->assertNull($obj->getEffectifMin());
+        $this->assertNull($obj->getNic());
+        $this->assertNull($obj->getSiret());
+        $this->assertNull($obj->getSiretFormate());
+        $this->assertNull($obj->getTrancheEffectif());
+        $this->assertNull($obj->getVille());
+
+        $this->assertNull($obj->getCodeNaf());
+        $this->assertNull($obj->getComplementAdresse());
+        $this->assertNull($obj->getDateCreation());
+        $this->assertNull($obj->getEtablissementCesse());
+        $this->assertNull($obj->getEtablissementEmployeur());
+        $this->assertNull($obj->getIndiceRepetition());
+        $this->assertNull($obj->getLatitude());
+        $this->assertNull($obj->getLibelleCodeNaf());
+        $this->assertNull($obj->getLibelleVoie());
+        $this->assertNull($obj->getLongitude());
+        $this->assertNull($obj->getNumeroVoie());
+        $this->assertNull($obj->getSiege());
+        $this->assertNull($obj->getTypeVoie());
+    }
+}
