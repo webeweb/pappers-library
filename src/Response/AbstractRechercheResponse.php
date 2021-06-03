57dabd569babcb271e8286b2721c4ca62a62e9ed
webeweb <support@webeweb.fr>
2021-06-03 19:39:31 +0200

    Add abstract Recherche response

---
+++ src/Response/AbstractRechercheResponse.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Response;
+
+use WBW\Library\Core\Model\Attribute\IntegerPageTrait;
+
+/**
+ * Abstract recherche response.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Response
+ */
+abstract class AbstractRechercheResponse extends AbstractResponse {
+
+    use IntegerPageTrait;
+
+    /**
+     * Total.
+     *
+     * @var int|null
+     */
+    private $total;
+
+    /**
+     * Constructor.
+     */
+    protected function __construct() {
+        parent::__construct();
+    }
+    /**
+     * Get the total.
+     *
+     * @return int|null Returns the total.
+     */
+    public function getTotal(): ?int {
+        return $this->total;
+    }
+
+    /**
+     * Set the total.
+     *
+     * @param int|null $total The total.
+     * @return AbstractRechercheResponse Returns this recherche response.
+     */
+    public function setTotal(?int $total): AbstractRechercheResponse {
+        $this->total = $total;
+        return $this;
+    }
+}
---
+++ tests/Response/AbstractRechercheResponseTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Response;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Response\TestRechercheResponse;
+
+/**
+ * Abstract recherche response test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Response
+ */
+class AbstractRechercheResponseTest extends AbstractTestCase {
+
+    /**
+     * Tests the setTotal() method.
+     *
+     * @return void
+     */
+    public function testSetTotal(): void {
+
+        $obj = new TestRechercheResponse();
+
+        $obj->setTotal(1);
+        $this->assertEquals(1, $obj->getTotal());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new TestRechercheResponse();
+
+        $this->assertNull($obj->getPage());
+
+        $this->assertNull($obj->getTotal());
+    }
+}
