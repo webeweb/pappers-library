aa63b730cbd7515dc131daa5fe90fab1b510ce56
webeweb <support@webeweb.fr>
2021-06-03 19:39:01 +0200

    Add abstract Request

---
+++ src/Request/AbstractRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Request;
+
+/**
+ * Abstract request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Request
+ * @abstract
+ */
+abstract class AbstractRequest {
+
+}
