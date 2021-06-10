d38e88f8f0965c9295b7e014c4b6473112e9f41d
webeweb <support@webeweb.fr>
2021-06-10 13:35:25 +0200

    Add Recherche publications request

---
+++ src/Request/RecherchePublicationsRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Request;
+
+/**
+ * Recherche publications request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Request
+ */
+class RecherchePublicationsRequest extends AbstractRequest {
+
+    /**
+     * Resources path.
+     *
+     * @var string
+     */
+    const RESOURCES_PATH = "/recherche-publications";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcesPath(): string {
+        return self::RESOURCES_PATH;
+    }
+}
---
+++ tests/Request/RecherchePublicationsRequestTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Request;
+
+use WBW\Library\Pappers\Request\RecherchePublicationsRequest;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Recherche publications request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Request
+ */
+class RecherchePublicationsRequestTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+
+    public function test__construct(): void {
+
+        $this->assertEquals("/recherche-publications", RecherchePublicationsRequest::RESOURCES_PATH);
+
+        $obj = new RecherchePublicationsRequest();
+
+        $this->assertEquals(RecherchePublicationsRequest::RESOURCES_PATH, $obj->getResourcesPath());
+    }
+}
