8b5de4981717e5a5d81632907c3dd7484368831e
webeweb <support@webeweb.fr>
2021-06-10 13:33:24 +0200

    Add Suggestions request

---
+++ src/Request/SuggestionsRequest.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Request;
+
+use WBW\Library\Pappers\Model\Attribute\StringQTrait;
+
+/**
+ * Suggestions request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Request
+ */
+class SuggestionsRequest extends AbstractRequest {
+
+    use StringQTrait;
+
+    /**
+     * Resources path.
+     *
+     * @var string
+     */
+    const RESOURCES_PATH = "/suggestions";
+
+    /**
+     * Cibles.
+     *
+     * @var string|null
+     */
+    private $cibles;
+
+    /**
+     * Longueur.
+     *
+     * @var int|null
+     */
+    private $longueur;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcesPath(): string {
+        return self::RESOURCES_PATH;
+    }
+
+    /**
+     * Get the cibles.
+     *
+     * @return string|null Returns the cibles.
+     */
+    public function getCibles(): ?string {
+        return $this->cibles;
+    }
+
+    /**
+     * Set the cibles.
+     *
+     * @param string|null $cibles The cibles.
+     * @return SuggestionsRequest Returns this suggestions request.
+     */
+    public function setCibles(?string $cibles): SuggestionsRequest {
+        $this->cibles = $cibles;
+        return $this;
+    }
+
+    /**
+     * Get the longueur.
+     *
+     * @return int|null Returns the longueur.
+     */
+    public function getLongueur(): ?int {
+        return $this->longueur;
+    }
+
+    /**
+     * Set the longueur.
+     *
+     * @param int|null $longueur The longueur.
+     * @return SuggestionsRequest Returns this suggestions request.
+     */
+    public function setLongueur(?int $longueur): SuggestionsRequest {
+        $this->longueur = $longueur;
+        return $this;
+    }
+}
---
+++ tests/Request/SuggestionsRequestTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Request;
+
+use WBW\Library\Pappers\Request\SuggestionsRequest;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Suggestions request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Request
+ */
+class SuggestionsRequestTest extends AbstractTestCase {
+
+    /**
+     * Tests the setCibles() method.
+     *
+     * @return void
+     */
+    public function testSetCibles(): void {
+
+        $obj = new SuggestionsRequest();
+
+        $obj->setCibles("cibles");
+        $this->assertEquals("cibles", $obj->getCibles());
+    }
+
+    /**
+     * Tests the setLongueur() method.
+     *
+     * @return void
+     */
+    public function testSetLongueur(): void {
+
+        $obj = new SuggestionsRequest();
+
+        $obj->setLongueur(1);
+        $this->assertEquals(1, $obj->getLongueur());
+    }
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+
+    public function test__construct(): void {
+
+        $this->assertEquals("/suggestions", SuggestionsRequest::RESOURCES_PATH);
+
+        $obj = new SuggestionsRequest();
+
+        $this->assertNull($obj->getQ());
+
+        $this->assertEquals(SuggestionsRequest::RESOURCES_PATH, $obj->getResourcesPath());
+
+        $this->assertNull($obj->getCibles());
+        $this->assertNull($obj->getLongueur());
+    }
+}
