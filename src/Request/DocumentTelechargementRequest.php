b4afd8b445eaf87262fbefe5e3a2cff3e39a0a12
webeweb <support@webeweb.fr>
2021-06-10 13:33:03 +0200

    Add Document téléchargement request

---
+++ src/Request/DocumentTelechargementRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Request;
+
+use WBW\Library\Pappers\Model\Attribute\StringTokenTrait;
+
+/**
+ * Document téléchargement request.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Request
+ */
+class DocumentTelechargementRequest extends AbstractRequest {
+
+    use StringTokenTrait;
+
+    /**
+     * Resources path.
+     *
+     * @var string
+     */
+    const RESOURCES_PATH = "/document/telechargement";
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResourcesPath(): string {
+        return self::RESOURCES_PATH;
+    }
+}
---
+++ tests/Request/DocumentTelechargementRequestTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Request;
+
+use WBW\Library\Pappers\Request\DocumentTelechargementRequest;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Document téléchargement request test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Request
+ */
+class DocumentTelechargementRequestTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+
+    public function test__construct(): void {
+
+        $this->assertEquals("/document/telechargement", DocumentTelechargementRequest::RESOURCES_PATH);
+
+        $obj = new DocumentTelechargementRequest();
+
+        $this->assertNull($obj->getToken());
+
+        $this->assertEquals(DocumentTelechargementRequest::RESOURCES_PATH, $obj->getResourcesPath());
+    }
+}
