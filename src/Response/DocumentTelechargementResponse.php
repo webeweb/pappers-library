8b562190028b99e4544ae6154099dcba5de7736a
webeweb <support@webeweb.fr>
2021-06-16 12:06:57 +0200

    Add Document téléchargement response

---
+++ src/Response/DocumentTelechargementResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Response;
+
+/**
+ * Document téléchargementResponse.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Response
+ */
+class DocumentTelechargementResponse extends AbstractResponse {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+}
