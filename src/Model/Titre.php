842432275eef76889ef7fe9708a6feefc2f8a781
webeweb <support@webeweb.fr>
2021-06-17 12:42:12 +0200

    Add Titre model

---
+++ src/Model/Titre.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model;
+
+use WBW\Library\Core\Model\Attribute\StringTypeTrait;
+use WBW\Library\Pappers\Model\Attribute\StringDecisionTrait;
+
+/**
+ * Titre.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model
+ */
+class Titre {
+
+    use StringDecisionTrait;
+    use StringTypeTrait;
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        // NOTHING TO DO
+    }
+}
---
+++ tests/Model/TitreTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model;
+
+use WBW\Library\Pappers\Model\Titre;
+use PHPUnit\Framework\TestCase;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Titre test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model
+ */
+class TitreTest extends AbstractTestCase {
+
+    /**
+     * Tests the __construct() method.
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new Titre();
+
+        $this->assertNull($obj->getDecision());
+        $this->assertNull($obj->getType());
+    }
+}
