a86283c26d7915e9b7a671cf76b6e8ced5cc901c
webeweb <support@webeweb.fr>
2021-06-22 12:56:32 +0200

    Add string ville naissance trait

---
+++ tests/Model/Attribute/StringVilleNaissanceTraitTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Model\Attribute;
+
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+use WBW\Library\Pappers\Tests\Fixtures\Model\Attribute\TestStringVilleNaissanceTrait;
+
+/**
+ * String ville naissance trait test.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Model\Attribute
+ */
+class StringVilleNaissanceTraitTest extends AbstractTestCase {
+
+    /**
+     * Tests the setVilleNaissance() method().
+     *
+     * @return void
+     */
+    public function testSetVilleNaissance(): void {
+
+        $obj = new TestStringVilleNaissanceTrait();
+
+        $obj->setVilleNaissance("villeNaissance");
+        $this->assertEquals("villeNaissance", $obj->getVilleNaissance());
+    }
+}
