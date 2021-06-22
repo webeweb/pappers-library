397e695f90652d63d61e6846a79da5aba7dcf1b5
webeweb <support@webeweb.fr>
2021-06-22 12:57:42 +0200

    Add string forme juridique trait

---
+++ src/Model/Attribute/StringFormeJuridiqueTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model\Attribute;
+
+/**
+ * String forme juridique trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model\Attribute
+ */
+trait StringFormeJuridiqueTrait {
+
+    /**
+     * Forme juridique.
+     *
+     * @var string|null
+     */
+    private $formeJuridique;
+
+    /**
+     * Get the forme juridique.
+     *
+     * @return string|null Returns the forme juridique.
+     */
+    public function getFormeJuridique(): ?string {
+        return $this->formeJuridique;
+    }
+
+    /**
+     * Set the forme juridique.
+     *
+     * @param string|null $formeJuridique The forme juridique.
+     * @return self Returns this instance.
+     */
+    public function setFormeJuridique(?string $formeJuridique): self {
+        $this->formeJuridique = $formeJuridique;
+        return $this;
+    }
+}
