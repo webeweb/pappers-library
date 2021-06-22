ccba2ba3c32830d8497e5160471f0ae6e7a3d3bb
webeweb <support@webeweb.fr>
2021-06-22 12:56:50 +0200

    Add string sigle trait

---
+++ src/Model/Attribute/StringSigleTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Model\Attribute;
+
+/**
+ * String sigle trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Model\Attribute
+ */
+trait StringSigleTrait {
+
+    /**
+     * Sigle.
+     *
+     * @var string|null
+     */
+    private $sigle;
+
+    /**
+     * Get the sigle.
+     *
+     * @return string|null Returns the sigle.
+     */
+    public function getSigle(): ?string {
+        return $this->sigle;
+    }
+
+    /**
+     * Set the sigle.
+     *
+     * @param string|null $sigle The sigle.
+     * @return self Returns this instance.
+     */
+    public function setSigle(?string $sigle): self {
+        $this->sigle = $sigle;
+        return $this;
+    }
+}
