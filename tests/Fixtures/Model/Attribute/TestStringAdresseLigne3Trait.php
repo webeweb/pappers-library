1ed1299f0be4e9f48214868d31cd3fa0a7138ef4
webeweb <support@webeweb.fr>
2021-06-22 12:55:24 +0200

    Add string adresse ligne 3 trait

---
+++ tests/Fixtures/Model/Attribute/TestStringAdresseLigne3Trait.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2021 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Fixtures\Model\Attribute;
+
+use WBW\Library\Pappers\Model\Attribute\StringAdresseLigne3Trait;
+
+/**
+ * Test string adresse ligne 3 trait.
+ *
+ * @author webeweb <https://github.com/webeweb/>
+ * @package WBW\Library\Pappers\Tests\Fixtures\Model\Attribute
+ */
+class TestStringAdresseLigne3Trait {
+
+    use StringAdresseLigne3Trait;
+}
