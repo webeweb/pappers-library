3702d9485494c40bdaf11416e7204fdbc8f37947
webeweb <support@webeweb.fr>
2023-02-06 17:51:55 +0100

    Add Document téléchargement response unit tests

---
+++ tests/Response/DocumentTelechargementResponseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the pappers-library package.
+ *
+ * (c) 2023 WEBEWEB
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WBW\Library\Pappers\Tests\Response;
+
+use Throwable;
+use WBW\Library\Pappers\Response\AbstractResponse;
+use WBW\Library\Pappers\Response\DocumentTelechargementResponse;
+use WBW\Library\Pappers\Tests\AbstractTestCase;
+
+/**
+ * Document téléchargement response test.
+ *
+ * @author webeweb <https://github.com/webeweb>
+ * @package WBW\Library\Pappers\Tests\Response
+ */
+class DocumentTelechargementResponseTest extends AbstractTestCase {
+
+    /**
+     * Tests saveAs()
+     *
+     * @return void
+     * @throws Throwable Throws an exception if an error occurs.
+     */
+    public function testSaveAs(): void {
+
+        // Set a filename mock.
+        $filename = __DIR__ . "/../../var/cache/DocumentTelechargementResponse.pdf";
+        if (true === file_exists($filename)) {
+            unlink($filename);
+        }
+
+        $obj = new DocumentTelechargementResponse();
+
+        $obj->saveAs($filename);
+        $this->assertFileExists($filename);
+    }
+
+    /**
+     * Tests __construct()
+     *
+     * @return void
+     */
+    public function test__construct(): void {
+
+        $obj = new DocumentTelechargementResponse();
+
+        $this->assertInstanceOf(AbstractResponse::class, $obj);
+    }
+}
